b995bfad02240870a8547c796944190b51461d9f
Harahorin <harahorinev@gmail.com>
2022-08-17 17:31:46 +0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView>
+      <Text>123</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
