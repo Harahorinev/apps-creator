c9919f8a1f48a031d8aae7952c5b6ee230d3dfcf
Harahorin <harahorinev@gmail.com>
2022-08-17 23:59:56 +0700

    added tabs and layout

--- navigation.tsx
+++ navigation.tsx
@@ -1,22 +1,28 @@
 import * as React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from '@react-navigation/native';
-import ProfileScreen from "./components/ProfileScreen";
-import SecondScreen from "./components/SecondScreen";
+import ProfilePage from "./screens/profilePage/ProfilePage";
+import NewsPage from "./screens/NewsPage";
+import MusicPage from "./screens/MusicPage";
+import DialogsPage from "./screens/dialogsPage/DialogsPage";
+import UserMessagesPage from "./screens/userMessagesPage/UserMessagesPage";
 
 
 const Drawer = createDrawerNavigator();
+// const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
     return (
         <NavigationContainer>
             <Drawer.Navigator useLegacyImplementation={true} initialRouteName="Profile">
-                <Drawer.Screen name="Profile" component={ProfileScreen} options={{headerShown: true}}/>
-                <Drawer.Screen name="Second" component={SecondScreen} options={{headerShown: true}}/>
+                <Drawer.Screen name="Profile" component={ProfilePage} options={{headerShown: true}}/>
+                <Drawer.Screen name="Dialogs" component={DialogsPage} options={{headerShown: true}}/>
+                <Drawer.Screen name="News" component={NewsPage} options={{headerShown: true}}/>
+                <Drawer.Screen name="Music" component={MusicPage} options={{headerShown: true}}/>
+                <Drawer.Screen name="Messages" component={UserMessagesPage} options={{headerShown: true}}/>
             </Drawer.Navigator>
         </NavigationContainer>
     )
 }
 
 export default RootNavigator
-// export default RootNavigator
